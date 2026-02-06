a45cfd8de21e15f917deebc50ab0d7ac965abc2c
marimuthum956-design <marimuthum956@gmail.com>
2026-02-06 16:51:05 +0530

    caddition and find data type

---
+++ mark.py
@@ -0,0 +1,7 @@
+m1=int(input("Enter mark:"))
+m2=int(input("Enter mark:"))
+m3=int(input("Enter mark:"))
+m4=int(input("Enter mark:"))
+m5=int(input("Enter mark:"))
+total=m1+m2+m3+m4+m5
+print("Total Mark:",total)
